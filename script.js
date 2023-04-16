23815a779022f2052c3597450688147ad17a7f3c
sukhbirkaushall <113819281+sukhbirkaushall@users.noreply.github.com>
2023-04-16 10:37:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Jani Door Gaye - Ustad Nusrat Fateh Ali Khan", filePath: "1.mp3", coverPath: "1.jpg"},
-    {songName: "Kinna sohna tenu rab ne banaya - Nusrat Fateh Ali Khan", filePath: "2.mp3", coverPath: "2.jpg"},
-    {songName: "Saadagi To Hamari Zara Dekhiye - Nusrat Fateh Ali Khan", filePath: "3.mp3", coverPath: "3.jpg"},
-    {songName: "Sanu Ik Pal Chain - Nusrat Fateh Ali Khan", filePath: "4.mp3", coverPath: "4.jpg"},
-    {songName: "Tere Bin Nahin Lagda- Nusrat Fateh Ali Khan", filePath: "5.mp3", coverPath: "5.jpg"},
-    {songName: "Ye Jo Halka Halka Saroor Hai - Ustad Nusrat Fateh Ali Khan", filePath: "2.mp3", coverPath: "6.jpg"},
-    {songName: "Kali Kali Zulfon Ke Phande - Nusrat Fateh Ali Khan", filePath: "2.mp3", coverPath: "7.jpg"},
-    {songName: "Tumhe Dillagi - Nusrat Fateh Ali Khan", filePath: "2.mp3", coverPath: "8.jpg"},
-    {songName: "Chaap Tilak Sab Cheen - Ustad Nusrat Fateh Ali Khan", filePath: "2.mp3", coverPath: "9.jpg"},
-    {songName: "Aisa Bana Sawarna Mubarak Tumhen - Nusrat Fateh Ali Khan", filePath: "4.mp3", coverPath: "10.jpg"},
+     {songName: "Jani Door Gaye", filePath: "1.mp3", coverPath: "1.jpg"},
+    {songName: "Kinna sohna tenu ", filePath: "2.mp3", coverPath: "2.jpg"},
+    {songName: "Saadagi To Hamari ", filePath: "3.mp3", coverPath: "3.jpg"},
+    {songName: "Sanu Ik Pal Chain", filePath: "4.mp3", coverPath: "4.jpg"},
+    {songName: "Tere Bin Nahin Lagda", filePath: "5.mp3", coverPath: "5.jpg"},
+    {songName: "Ye Jo Halka Halka Suroor", filePath: "2.mp3", coverPath: "6.jpg"},
+    {songName: "Kali Kali Zulfon Ke Phande", filePath: "2.mp3", coverPath: "7.jpg"},
+    {songName: "Tumhe Dillagi", filePath: "2.mp3", coverPath: "8.jpg"},
+    {songName: "Chaap Tilak Sab Cheen", filePath: "2.mp3", coverPath: "9.jpg"},
+    {songName: "Aisa Bana Sawarna ", filePath: "4.mp3", coverPath: "10.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
